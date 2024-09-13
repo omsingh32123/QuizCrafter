0c736d5cc9090d3d79b1440df061c4c294e9c5b4
Om Singh <omsingh32123@gmail.com>
2024-09-13 06:09:21 +0530

    corrected env var

--- quiz-crafter/src/App.jsx
+++ quiz-crafter/src/App.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React,{ useState, useEffect } from "react";
 import Home from './Pages/Home/Home';
-
+import Invalid from "./Pages/Invalid/Invalid";
 function App() {
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  const updateScreenWidth = () => {
+    setScreenWidth(window.innerWidth);
+  };
+  useEffect(() => {
+    window.addEventListener('resize', updateScreenWidth);
+    return () => {
+      window.removeEventListener('resize', updateScreenWidth);
+    };
+  }, []);
+  if(screenWidth<600)
+  {
+    return(
+      <Invalid />
+    );
+  }
   return (
-    <Home />
+      <Home />
   );
 }
 
---
+++ quiz-crafter/src/Pages/Invalid/Invalid.jsx
@@ -0,0 +1,16 @@
+import React, { useState, useRef } from "react";
+import './../Home/Home.css';
+
+const Invalid = () => {
+    
+    return (
+        <div className="home-container-1">
+            <h1 className="home-text">
+                <span className="home-word-l">Operable on</span>
+                <span className="home-word-r">bigger screens</span>
+                <span className="home-word-l">only</span>
+            </h1>
+        </div>
+    );
+};
+export default Invalid;
